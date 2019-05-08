85ef701e78f1373414f83c1e60aa82f29d8f6147
Willy Chen <willy700628@gmail.com>
2019-05-08 17:27:12 +0800

    Android HomeWork 1

---
+++ app/src/main/java/tw/com/yuantsung/guess/MaterialActivity.kt
@@ -0,0 +1,62 @@
+package tw.com.yuantsung.guess
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_material.*
+import kotlinx.android.synthetic.main.content_material.*
+
+class MaterialActivity : AppCompatActivity() {
+
+    var secretNumber = SecretNumber()
+    val TAG = MaterialActivity::class.java.simpleName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_material)
+        setSupportActionBar(toolbar)
+        Log.d(TAG, "secret: ${secretNumber.secret}")
+        fab.setOnClickListener { view ->
+            AlertDialog.Builder(this)
+                .setTitle("Replay Game")
+                .setMessage("Are you sure?")
+                .setNegativeButton(getString(R.string.ok),{dialog, which ->
+                    secretNumber.reset()
+                    counter.setText(secretNumber.count.toString())
+                    numberText.setText(null)
+                })
+                .setNeutralButton("cancel",null)
+                .show()
+
+        }
+        counter.setText(secretNumber.count.toString())
+    }
+
+
+    fun okBtnAction(view: View) {
+        val number = numberText.text.toString().toInt()
+        Log.d(TAG, "number: $number}")
+        val diff = secretNumber.validate(number)
+        var message = getString(R.string.Yes_got)
+        if (diff < 0) {
+            message = getString(R.string.bigger)
+        } else if (diff > 0) {
+            message = getString(R.string.smaller)
+        } else if (secretNumber.count < 3) {
+            message = getString(R.string.excellent) + number
+        }
+        counter.setText(secretNumber.count.toString())
+//        Toast.makeText(this,message,Toast.LENGTH_LONG).show()
+        AlertDialog.Builder(this)
+            .setTitle(getString(R.string.dialog_Title))
+            .setMessage(message)
+            .setPositiveButton(getString(R.string.ok), null)
+            .show()
+    }
+}
